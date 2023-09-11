990f08868d9bda6503e019b99e6c6e027600d192
kyu-wooon <122020452+kyu-wooon@users.noreply.github.com>
2023-09-11 14:30:56 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int a = 0; a<=n; a++){
+            if(a%2 == 0){answer = answer + a;}
+            
+            
+            
+        }
+        return answer;
+    }
+}
