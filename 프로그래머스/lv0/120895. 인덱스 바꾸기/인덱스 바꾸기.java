3379501130f26d0762c7ee4f42a128869451aea2
kyu-wooon <122020452+kyu-wooon@users.noreply.github.com>
2023-09-11 14:43:19 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        String imsi = my_string;
+        
+        StringBuilder sb = new StringBuilder(my_string);
+        sb.setCharAt(num1,my_string.charAt(num2));
+        sb.setCharAt(num2,my_string.charAt(num1));
+        answer = sb.toString();
+
+        return answer;
+    }
+}
