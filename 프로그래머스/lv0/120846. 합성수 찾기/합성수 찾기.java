bdb5e31103298cae6e45d771f9b271e847e7d4ef
kyu-wooon <122020452+kyu-wooon@users.noreply.github.com>
2023-09-11 14:47:45 +0900

    [level 0] Title: 합성수 찾기, Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int count = 0; 
+        
+        for(int i = 1; i<= n ; i++){
+            for(int j = 1; j <=i; j++){
+                if(i%j==0){
+                    count++;
+                }
+                
+            }
+            if(count>2){
+                    answer++;
+                    count = 0;
+                }else{count = 0; }
+            
+        }
+        return answer;
+    }
+}
