70f55171d107102740c5e3dc916dd5d997f201f8
kyu-wooon <122020452+kyu-wooon@users.noreply.github.com>
2023-09-12 15:20:30 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int a = (brown+4)/2;
+        int b = brown+yellow;
+        int[] answer = {(int)(a+Math.sqrt(a*a-4*b))/2,(int)(a-Math.sqrt(a*a-4*b))/2};
+        return answer;
+    }
+}
