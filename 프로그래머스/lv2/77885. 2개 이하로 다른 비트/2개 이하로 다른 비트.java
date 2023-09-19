8f89a961ea8996c275cbe60099ec361667517785
kyu-wooon <122020452+kyu-wooon@users.noreply.github.com>
2023-09-19 15:27:40 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 153.03 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -4,21 +4,35 @@ class Solution {
         long[] answer = new long[numbers.length];
 
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 4 != 3) {
+            //짝수인 경우 
+            if (numbers[i] % 2  == 0) {
                 answer[i] = numbers[i] + 1;
                 continue;
-            }
-
+            }else{
+                
+            //홀수인 경우 
             String bit = Long.toBinaryString(numbers[i]);
             if (!bit.contains("0")) { // 0이 없으면
+                //앞에 0을 붙인 후, 앞과 뒤의 순서를 바꾼다. 
+               // System.out.println("0 없는 이전 bit : " + bit);
                 bit = 0 + bit;
+              //  System.out.println("중간 bit : " + bit);
                 bit = String.valueOf(bit.charAt(1)) + bit.charAt(0) + bit.substring(2);
-            }else{
-                int x = bit.lastIndexOf("0");
+               // System.out.println("이후 bit : " + bit);
+            }else{//0이 있으면 
+               // System.out.println("0 있는 이전 bit : " + bit);
+                int x = bit.lastIndexOf("0");//마지막으로 0이 있는 인덱스 
+                //System.out.println("0 있는 이전 bit x : " + x);
+               // System.out.println("bit substring : " + bit.substring(0,x));
+                //처음부터 0이 있는 인덱스 전까지 잘라내기 +  10 + 0이 있는 인덱스 +2부터 끝까지 
                 bit = bit.substring(0, x) + "10" + bit.substring(x + 2);
+                // System.out.println("이후 bit : " + bit);
             }
 
             answer[i] = toBinary(bit);
+                
+            }
+
 
         }
 
