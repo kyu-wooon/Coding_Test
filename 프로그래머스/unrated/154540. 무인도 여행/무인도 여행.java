112a38546f31cfb2597b888dca315c061ba46ad4
kyu-wooon <122020452+kyu-wooon@users.noreply.github.com>
2023-10-05 13:53:20 +0900

    [unrated] Title: 무인도 여행, Time: 2.60 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.java
@@ -1,76 +1,56 @@
 import java.util.*;
-
 class Solution {
-
-    public List<Integer> ansList;
-
-    public int[] solution(String[] inputMap) {
-        //return type is int[]
-        ansList = new ArrayList<>();
-
-        int[][] map = new int[inputMap.length][inputMap[0].length()];
-
-        for(int i=0; i<inputMap.length; i++){
-            String elem = inputMap[i];
-            for(int j=0; j<elem.length(); j++){
-                String target = elem.substring(j,j+1);
-                if(target.equals("X")){
-                    map[i][j] = -1;
+    static List<Integer> answer = new ArrayList<>();
+    static int width;
+    static int height;
+    static char[][] map ;// 지도
+    static int[][] visited ;//방문여부 저장
+    static int[] xdir={0,0,-1,1};
+    static int[] ydir={-1,1,0,0};
+    
+    int bfs(int x, int y){
+        int temp=map[x][y]-'0';
+        for(int k=0;k<4;k++){//상하좌우 4방향
+            int dx=x+xdir[k];
+            int dy=y+ydir[k];
+            if(dx >= 0 && dx < height && dy >= 0 && dy < width){
+                if(visited[dx][dy] == 0 && map[dx][dy] != 'X'){
+                    visited[dx][dy]=1;
+                    temp+=bfs(dx,dy);    
                 }
-                else map[i][j] = Integer.parseInt( target );
-            }
+            } 
+        }
+        return temp;
+    }
+    
+    public int[] solution(String[] maps) {
+        width=maps[0].length();
+        height=maps.length;
+        map = new char[maps.length][];
+        visited = new int[maps.length][maps[0].length()];
+        
+        //지도 초기화
+        for(int i=0;i<maps.length;i++){
+            map[i]=maps[i].toCharArray();
         }
 
-        boolean[][] visited = new boolean[map.length][map[0].length];
-
-        //
-        for(int i=0; i<map.length; i++){
-            for(int j=0; j<map[0].length; j++){
-                if(map[i][j] != -1 && visited[i][j] == false){
-                    // 정답 리스트에 0을 추가한다.
-                    ansList.add(0);
-
-                    // dfs 돌린다.
-                    dfs(visited, i, j, map);
+        //지도에서 방문하지 않은 무인도를 방문 
+         for(int i=0;i<maps.length;i++){
+            for(int j=0;j<map[i].length;j++){
+                if(map[i][j] != 'X' && visited[i][j]==0){
+                    visited[i][j]=1;
+                    int isLand=bfs(i,j);
+                    if (isLand != 0) {
+                        answer.add(isLand);
+                    }
                 }
             }
         }
-
-
-        if(ansList.isEmpty()) return new int[]{-1};
-        else {
-            Collections.sort(ansList);
-            int[] ans = new int[ansList.size()];
-            for(int i=0; i<ans.length; i++){
-                ans[i] = ansList.get(i);
-            }
-            return ans;
+        Collections.sort(answer);
+        
+        if(answer.isEmpty()){
+            answer.add(-1);
         }
-    }//main
-
-    public void dfs(boolean[][] visited, int row, int col, int[][] map){
-        // 범위 벗어나면 아웃
-        if(
-            row < 0 || row > map.length-1 || col < 0 || col > map[0].length-1
-        ) return;
-        // -1이면 아웃
-        if (map[row][col]==-1) return;
-        // 이미 방문했으면 아웃
-        if(visited[row][col]) return;
-
-        // 방문 체크
-        visited[row][col] = true;
-
-        // 리스트의 마지막 요소에 현 요소를 누적
-        int val = ansList.get(ansList.size()-1);
-        val += map[row][col];
-        ansList.set(ansList.size()-1, val);
-
-        // 동서남북 호출
-        dfs(visited, row, col+1, map);
-        dfs(visited, row, col-1, map);
-        dfs(visited, row+1, col, map);
-        dfs(visited, row-1, col, map);
-    }//func
-
-}//main class
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+}
